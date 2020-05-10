48b2ddea5be07ebb920a15aa903407788d56a163
Sefat Anam <sefatanam@gmail.com>
2020-05-11 01:01:50 +0600

    dependencies added

---
+++ screens/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+const Profile = () => {
+  return (
+    <View>
+      <Text>From profile</Text>
+    </View>
+  );
+};
+export default Profile;
+const styles = StyleSheet.create({});
