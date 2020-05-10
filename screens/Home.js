fbf3fdfdfaa99d13dd1f6e611f260928186e0f44
Sefat Anam <sefatanam@gmail.com>
2020-05-11 02:40:44 +0600

    cycle 1 of half

--- screens/Home.js
+++ screens/Home.js
@@ -2,25 +2,46 @@ import React from "react";
 import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 import { Card, FAB } from "react-native-paper";
 
-const Home = ({navigation}) => {
+const Home = ({ navigation }) => {
   const data = [
-    { id: 1, name: "William Sierra", position: "React-native Developer" },
-    { id: 2, name: "William Sierra", position: "React-native Developer" },
-    // { id: 3, name: "William Sierra", position: "React-native Developer" },
-    // { id: 4, name: "William Sierra", position: "React-native Developer" },
-    // { id: 5, name: "William Sierra", position: "React-native Developer" },
-    // { id: 6, name: "William Sierra", position: "React-native Developer" },
-    // { id: 7, name: "William Sierra", position: "React-native Developer" },
-    // { id: 8, name: "William Sierra", position: "React-native Developer" },
-    // { id: 9, name: "William Sierra", position: "React-native Developer" },
-    // { id: 10, name: "William Sierra", position: "React-native Developer" },
-    // { id: 11, name: "William Sierra", position: "React-native Developer" },
-    // { id: 12, name: "William Sierra", position: "React-native Developer" },
+    {
+      id: "1",
+      name: "William Sierra",
+      email: "William@email.com",
+      salary: "112.7k",
+      phone: "6464565436",
+      position: "React-native Developer",
+      picture:
+        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    },
+    {
+      id: "2",
+      name: "Madison Kyth",
+      email: "Madison@email.com",
+      salary: "89.7k",
+      phone: "6464565436",
+      position: "Angular Developer",
+      picture:
+        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    },
+    {
+      id: "3",
+      name: "Json Kathiren ",
+      email: "Kathiren@email.com",
+      salary: "65.7k",
+      phone: "6464565436",
+      position: "Angular Developer",
+      picture:
+        "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    },
   ];
 
   const renderList = (item) => {
     return (
-      <Card style={styles.myCard} onPress={()=>navigation.navigate('Profile')}>
+      <Card
+        style={styles.myCard}
+        onPress={() => navigation.navigate("Profile", { item })}
+      >
         <View style={styles.cardView}>
           <Image
             style={styles.imageView}
@@ -38,19 +59,19 @@ const Home = ({navigation}) => {
     );
   };
   return (
-    <View style={{flex:1}}>
+    <View style={{ flex: 1 }}>
       <FlatList
         data={data}
-        keyExtractor={(item) => `${item.id}`}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => {
           return renderList(item);
         }}
       />
-      <FAB onPress={()=>navigation.navigate('Create')}
+      <FAB
+        onPress={() => navigation.navigate("Create")}
         style={styles.fab}
         small={false}
         icon="plus"
-       
       />
     </View>
   );
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,17 +1,27 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, Text, View, Image,Linking ,Platform} from "react-native";
+import { StyleSheet, Text, View, Image, Linking, Platform } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { Title, Card, Button } from "react-native-paper";
 import { MaterialIcons, Entypo } from "@expo/vector-icons";
-const Profile = () => {
-    const openDial=()=>{
-        if (Platform.OS=="android") {
-            Linking.openURL("tel:12345")
-        }else{
-            Linking.openURL('telprompt:123456')
-        }
+
+const Profile = (props) => {
+  const {
+    id,
+    name,
+    salary,
+    phone,
+    picture,
+    position,
+    email,
+  } = props.route.params.item;
+  const openDial = () => {
+    if (Platform.OS == "android") {
+      Linking.openURL("tel:12345");
+    } else {
+      Linking.openURL("telprompt:123456");
     }
+  };
   return (
     <View style={styles.root}>
       <LinearGradient
@@ -22,52 +32,55 @@ const Profile = () => {
         <Image
           style={styles.imageView}
           source={{
-            uri:
-              "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            uri: picture,
           }}
         />
       </View>
       <View style={styles.titleContainer}>
-        <Title>William Saierra</Title>
-        <Text>React-Native Developer</Text>
+        <Title>{name}</Title>
+        <Text>{position}</Text>
       </View>
 
-      <Card style={styles.myCard} onPress={()=>Linking.openURL("mailto:sefatanam@gmail.com")}>
+      <Card
+        style={styles.myCard}
+        onPress={() => Linking.openURL("mailto:sefatanam@gmail.com")}
+      >
         <View style={styles.cardContent}>
           <MaterialIcons name="email" size={32} color="#6420ee" />
-          <Text style={styles.text}>someone@email.com</Text>
+          <Text style={styles.text}>{email}</Text>
         </View>
       </Card>
       {/* ==== */}
-      <Card style={styles.myCard} onPress={()=>openDial()}>
+      <Card style={styles.myCard} onPress={() => openDial()}>
         <View style={styles.cardContent}>
           <Entypo name="phone" size={32} color="#6420ee" />
-          <Text style={styles.text}>+98 11122233</Text>
+          <Text style={styles.text}>{phone}</Text>
         </View>
       </Card>
       {/* ==== */}
       <Card style={styles.myCard}>
         <View style={styles.cardContent}>
           <MaterialIcons name="attach-money" size={32} color="#6420ee" />
-          <Text style={styles.text}> 112.8k</Text>
+          <Text style={styles.text}> {salary}</Text>
         </View>
       </Card>
       <View style={styles.buttonContainer}>
-      <Button
-        style={styles.inputStyle}
-        icon="account-edit"
-        mode="contained"
-        onPress={() => alert('edit')}
-      >
-        Update
-      </Button>
-      <Button
-        style={styles.inputStyle}
-        icon="account-edit"
-        mode="contained"
-        onPress={() => alert('delete')}
-      >
-Delete      </Button>
+        <Button
+          style={styles.inputStyle}
+          icon="account-edit"
+          mode="contained"
+          onPress={() => alert("edit")}
+        >
+          Update
+        </Button>
+        <Button
+          style={styles.inputStyle}
+          icon="account-edit"
+          mode="contained"
+          onPress={() => alert("delete")}
+        >
+          Delete{" "}
+        </Button>
       </View>
     </View>
   );
@@ -88,7 +101,7 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     alignItems: "center",
-    margin:10
+    margin: 10,
   },
   myCard: { margin: 3 },
   cardContent: { flexDirection: "row", padding: 8 },
@@ -96,8 +109,10 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 3,
     marginLeft: 5,
-  },buttonContainer:{
-      flexDirection:'row',
-      justifyContent:'space-around',padding:10
-  }
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
 });
