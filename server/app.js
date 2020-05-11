90d6f5b1829ecd4c52377f0b21cc69ae73fe6974
Sefat Anam <sefatanam@gmail.com>
2020-05-11 17:12:52 +0600

    nodejs server + ngrok installed

---
+++ server/app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("./Employee");
+
+app.use(bodyParser.json());
+
+const Employee = mongoose.model("employee");
+
+// const password =rPKMMHEtEVZI3TTb;
+const mongoUri =
+  "mongodb+srv://cnq:rPKMMHEtEVZI3TTb@cluster0-audg9.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connect to mongo success");
+});
+mongoose.connection.on("error", () => {
+  console.log("connect to mongo error");
+});
+
+app.get("/", (req, res) => {
+    Employee.find({}).then(data=>{
+        res.send(data)
+    }).catch(err=>{
+        res.send(err)
+    })
+//   res.send("Welcome to node js");
+});
+
+app.post("/send-data", (req, res) => {
+  const employee = new Employee({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    picture: req.body.picture,
+    salary: req.body.salary,
+    position: req.body.position,
+  });
+
+  employee
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/delete", (req, res) => {
+  Employee.findByIdAndRemove(req.body.id)
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/update", (req, res) => {
+  Employee.findByIdAndUpdate(req.body.id, {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    picture: req.body.picture,
+    salary: req.body.salary,
+    position: req.body.position,
+  })
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("server-running");
+});
